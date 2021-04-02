87367708605a1fe8553c7636e47e2623d47edfe1
Alkaid <alkaidwe@gmail.com>
2021-04-02 10:28:46 +0800

    refactor User's model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,42 +2,35 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name',
+        'eth_address',
         'email',
-        'password',
+        'twitter',
+        'telegram',
+        'domain',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'id',
+        'eth_address',
+        'created_at',
+        'updated_at',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+    protected $appends = [
+        'address'
     ];
+
+    public function getAddressAttribute(){
+        return $this->attributes['eth_address'];
+    }
 }
