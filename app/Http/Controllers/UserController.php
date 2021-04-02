ee6c30287869e3ac71d2610027ade6dc2185b427
Alkaid <alkaidwe@gmail.com>
2021-04-02 10:49:35 +0800

    added UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\BadRequestException;
+use App\Helpers\EthereumValidator;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Pelieth\LaravelEcrecover\EthSigRecover;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     * @throws BadRequestException
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'eth_address' => 'required|unique:users,eth_address|size:42|starts_with:0x',
+            'email' => 'required|email',
+            'twitter' => 'required',
+            'telegram' => 'required|starts_with:@',
+            'domain' => 'required',
+            'sign' => 'required|size:132|starts_with:0x',
+        ]);
+
+        // check address
+        $ethValidator = new EthereumValidator();
+        $address = $request->input('eth_address');
+        if(!$ethValidator->isAddress($address)) {
+            throw new BadRequestException('Invalid Address');
+        }
+
+        // verify signature
+        $eth_sig_util = new EthSigRecover();
+        $rawAddress = hex2bin(substr($address, 2));
+        $sign = $request->input('sign');
+        $recoverAddr = $eth_sig_util->personal_ecRecover($rawAddress, $sign);
+        if (strtolower($address) !== strtolower($recoverAddr)) {
+            throw new BadRequestException('Invalid Signature');
+        }
+
+        $user = User::create($request->only([
+            'eth_address',
+            'email',
+            'twitter',
+            'telegram',
+            'domain',
+        ]));
+
+        return response($user);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param string $address
+     * @return \Illuminate\Http\Response
+     * @throws BadRequestException
+     */
+    public function show($address)
+    {
+        $ethValidator = new EthereumValidator();
+        if(!$ethValidator->isAddress($address)) {
+            throw new BadRequestException('Invalid Address');
+        }
+
+        return User::findByAddress($address);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
