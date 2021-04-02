81a92829eaf95b384a783ae79d405ffcbe302b4c
Alkaid <alkaidwe@gmail.com>
2021-04-02 10:28:16 +0800

    handle exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +38,14 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (Exception $e) {
+            if ($e instanceof ValidationException) {
+                return response($e->errors(), $e->status);
+            }
+
+            return response($e->getMessage(), 400);
         });
     }
 }
