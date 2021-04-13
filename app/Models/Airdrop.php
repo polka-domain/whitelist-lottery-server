db527d7b9bd86481a24c0836fe0c4fc53a3255bb
Alkaid <alkaidwe@gmail.com>
2021-04-13 23:23:11 +0800

    create model Airdrop

---
+++ app/Models/Airdrop.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Airdrop extends Model
+{
+    protected $fillable = [
+        'eth_address',
+    ];
+
+    protected $hidden = [
+        'id',
+        'eth_address',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $appends = [
+        'address'
+    ];
+
+    public function getAddressAttribute(){
+        return $this->attributes['eth_address'];
+    }
+
+    public static function findByAddress($address) {
+        return self::where('eth_address', $address)->first();
+    }
+}
