de55f3819ec6b60b607447363bfb2eb05abc0638
Alkaid <alkaidwe@gmail.com>
2021-04-13 23:22:42 +0800

    create table airdrops

---
+++ database/migrations/2021_04_13_110229_create_airdrops_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAirdropsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('airdrops', function (Blueprint $table) {
+            $table->id();
+            $table->string('eth_address')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('airdrops');
+    }
+}
